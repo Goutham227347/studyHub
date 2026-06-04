13b205295808cc1d320e569416be3eb5b16298de
anand6605 <anandvkpb@gmail.com>
2026-06-04 23:34:16 +0530

    Add type hints to notify_user function for better code documentation

--- core/utils.py
+++ core/utils.py
@@ -1,8 +1,28 @@
+from typing import Optional
+from django.contrib.auth.models import User
+
 from .models import Notification
 
 
-def notify_user(recipient, title, message, notification_type='system', link=''):
-    """Create a notification for a user."""
+def notify_user(
+    recipient: User,
+    title: str,
+    message: str,
+    notification_type: str = 'system',
+    link: str = ''
+) -> Notification:
+    """Create a notification for a user.
+    
+    Args:
+        recipient: The User object to receive the notification
+        title: Notification title (max 200 chars)
+        message: Notification message/body
+        notification_type: Type of notification (like, comment, download, approval, rejection, system)
+        link: Optional URL link for the notification
+        
+    Returns:
+        The created Notification object
+    """
     return Notification.objects.create(
         recipient=recipient,
         title=title,
